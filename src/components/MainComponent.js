9bf42c92094e7c575a21e9413d7997027146fd0c
virattt <virat.dot@gmail.com>
2019-04-03 15:00:26 -0700

    Completed Homework 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
@@ -44,6 +45,12 @@ class Main extends Component {
             );
         }
 
+        const AboutUs = () => {
+            return (
+                <About leaders={this.state.leaders} />
+            )
+        }
+
         return (
             <div>
                 <Header />
@@ -51,6 +58,7 @@ class Main extends Component {
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
                     <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route exact path="/aboutus" component={AboutUs} />
                     <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
